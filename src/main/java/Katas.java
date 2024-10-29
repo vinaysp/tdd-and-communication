3bd9c6ad308b35ad3a1c544ad726c22b9e5a92ec
yee0802 <NovusAa12@proton.me>
2024-10-29 12:38:28 +0000

    feat: implement calculateMean method functionality

--- src/main/java/Katas.java
+++ src/main/java/Katas.java
@@ -29,6 +29,18 @@ public class Katas {
         return result;
     }
 
+    public double calculateMean(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0.0;
+        }
 
+        int numsLength = nums.length;
+        int sum = 0;
 
+        for (int num : nums) {
+            sum += num;
+        }
+
+        return (double) sum / numsLength;
+    }
 }
