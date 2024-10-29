eeaad39a44d0db1c12c608f35d23b4ba187c7a48
Vinay Patel <vinayspatel0@gmail.com>
2024-10-29 12:14:52 +0000

    feat: implement logic for sumMiddleNumbers method

--- src/main/java/Katas.java
+++ src/main/java/Katas.java
@@ -1,7 +1,32 @@
 public class Katas {
 
     public int sumMiddleNumbers(int[] nums){
-        return 0;
+        int result = 0;
+
+        if (nums == null || nums.length < 3){
+            return result;
+        }
+
+        int lowestNumber = nums[0];
+        int highestNumber = 0;
+
+        for ( int num : nums){
+            if (num < lowestNumber){
+                lowestNumber = num;
+            }
+
+            if (num > highestNumber){
+                highestNumber = num;
+            }
+        }
+
+        for (int num: nums){
+            if ( num > lowestNumber && num < highestNumber){
+                result += num;
+            }
+        }
+
+        return result;
     }
 
 
